9b6378ea2ad0f4d9fe1a3f0ef3b601ccfac39144
miguelpantoja89 <mgbastechnology@gmail.com>
2020-07-21 12:39:09 +0200

    creating a reactive form with validators

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MailComponent } from './components/mail/mail.component';
 import { MailListComponent } from './components/mail-list/mail-list.component';
+import { NewMailComponent } from './components/new-mail/new-mail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MailComponent,
-    MailListComponent
+    MailListComponent,
+    NewMailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/new-mail/new-mail.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-new-mail',
+  templateUrl: './new-mail.component.html',
+  styleUrls: ['./new-mail.component.scss']
+})
+export class NewMailComponent implements OnInit {
+  newMail: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  
+  ngOnInit(): void {
+    this.newMail = this.formBuilder.group({
+      title: ['', [Validators.required, Validators.minLength(3)]],
+      body: ['', [Validators.required, Validators.minLength(10)]],
+      to: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  get form() { return this.newMail.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.newMail.invalid) {
+        return;
+    }
+
+    let mail = this.newMail.value;
+    mail.read= false;
+    mail.from= 'sender1@gmail.com';
+
+    alert("Email Sent \nRemoving form");
+    this.onReset();
+}
+
+onReset() {
+    this.submitted = false;
+    this.newMail.reset();
+}
+}
